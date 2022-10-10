d0c82b5347d09f4f0ca45b309601efd73576b3b8
pauloricardoprsm <pauloricardoprsm@gmail.com>
2022-10-10 16:21:01 -0300

    Passagem de paramentros em um componente JSX

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => 
+    <React.Fragment>
+            <h1>Primeiro</h1>
+            <h2>Exemplo de um componente</h2>
+    </React.Fragment>
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import './index.css'
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Primeiro from './components/Primeiro'
+import ComParametros from './components/ComParametros'
+
 ReactDOM.render( 
-    <h1>Paulo</h1>,
+    <div>
+        <Primeiro/>
+        <ComParametros titulo="Esse é o título"
+           subtitulo="Esse é o subtitulo 2"></ComParametros>
+    </div>,
     document.getElementById('root')
 )
