23fd9b21d231980fc04ba0d7bfdd34823e422ff8
pauloricardoprsm <pauloricardoprsm@gmail.com>
2022-10-10 16:25:59 -0300

    Renderização de componentes com filhos

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import './index.css'
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Primeiro from './components/Primeiro'
-import ComParametros from './components/ComParametros'
+// import Primeiro from './components/Primeiro'
+// import ComParametros from './components/ComParametros'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render( 
     <div>
-        <Primeiro/>
+        <ComFilhos>
+                <li>Paulo</li>
+                <li>Ricardo</li>
+                <li>Siqueira</li>
+        </ComFilhos>
+        {/*<Primeiro/>
         <ComParametros titulo="Esse é o título"
            subtitulo="Esse é o subtitulo 2"></ComParametros>
+        */}
     </div>,
     document.getElementById('root')
 )
